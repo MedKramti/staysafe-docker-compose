ff32db6b2146a0ffe7a54d3245c5dd72da7bc84a
Kramti <mohamedkramti99@gmail.com>
2023-11-12 11:42:08 +0100

    Update fromend application and dataset

--- databases/authserver_dataset/users.sql
+++ databases/authserver_dataset/users.sql
@@ -20,9 +20,6 @@ ALTER TABLE users ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 COPY users (id, username, password, role) FROM stdin;
 2	admin	$2a$12$/aJH3jFpsU0z2G.NL/Gp1ennn5ETlkzDvlAD5Rh6K2G68JQW3y8BK	admin
-1	kramti	$2a$10$GuGm9JvsZc8ieXBORw4QZeow/QY8WVit3DhVv8by/2pafdT68hStC	user
-3	sdsd	$2a$10$4R/XNLL4pFJ6N.QGBNndOupp2H6geOoaBGiOqKJqKaNIp8g1Y6ICu	user
-103	demo	$2a$10$DldcbIAhjGO5Bv/KK0r31eVCNAPV1d2/BbI0FG7ESqYEM4VAJ5OAm	user
-104	demoaccount	$2a$10$woV/nHiM1LIlbMB7thSkQu95Z7afaBL0Y7bkK8o0/dq51HAzFZQla	user
+1	demo	$2a$12$zO5NlC347Zr1vd8wLOTS8edwtELMk67jUNIh4ZoeMvG1fV7Wmupvi	user
 \.
 
